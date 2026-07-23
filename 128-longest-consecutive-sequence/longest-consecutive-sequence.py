830ae095779a7a5c55afff13841f12950162ff05
Sagar Chandra <sagarchandra1824@gmail.com>
2026-07-23 23:56:59 +0530

    Time: 45 ms (89.54%) | Memory: 27.1 MB (17.92%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.py
@@ -4,17 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
-        left=0
-
+        num_set=set(nums)
         max_len=0
-        num_set=sorted(set(nums))
-        for right in range(1,len(num_set)):
-            if num_set[right]-1==num_set[right-1]:
-                max_len=max(max_len,right-left+1)
-                continue
-            else:
-                left=right
-        return max_len if max_len!=0 else 1
+        for n in num_set:
+            if n-1 not in num_set:
+                length=1
+                while n+1 in num_set:
+                    length+=1
+                    n+=1
+                max_len=max(max_len,length)
+        return max_len
         

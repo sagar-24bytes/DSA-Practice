dbb866a6bba2974acb081d32262bd59bbef9cf38
Sagar Chandra <sagarchandra1824@gmail.com>
2026-08-01 00:34:46 +0530

    Time: 94 ms (83.28%) | Memory: 20.2 MB (23.08%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -1,14 +1,13 @@
 from collections import Counter
 class Solution:
     def minimumPushes(self, word: str) -> int:
-        ans=0
-        idx=0
+        n=len(word)
+        if n<=8:
+            return n
         phone=Counter(word)
         freq=sorted(phone.values(),reverse=True)
-        for x in freq:
-            mul=(idx//8)+1
-            ans+=(mul*x)
-            idx+=1
+        ans=0
+        for i,x in enumerate(freq):
+            ans+=((i//8)+1)*x
         return ans
-            
         

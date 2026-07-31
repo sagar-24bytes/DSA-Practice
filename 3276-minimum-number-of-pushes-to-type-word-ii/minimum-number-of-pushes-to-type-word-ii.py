3c7767f3a1a6d4a876baacc832d32be56900a416
Sagar Chandra <sagarchandra1824@gmail.com>
2026-08-01 00:24:32 +0530

    Time: 96 ms (76.59%) | Memory: 20.1 MB (23.08%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.py
@@ -6,16 +6,9 @@ class Solution:
         phone=Counter(word)
         freq=sorted(phone.values(),reverse=True)
         for x in freq:
-            idx+=1
-            if 1<=idx<=8:
-                mul=1
-            elif 9<=idx<=16:
-                mul=2
-            elif 17<=idx<=24:
-                mul=3
-            else:
-                mul=4
+            mul=(idx//8)+1
             ans+=(mul*x)
+            idx+=1
         return ans
             
         

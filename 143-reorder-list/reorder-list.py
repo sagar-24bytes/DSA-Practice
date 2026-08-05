55818a4ad53c7cca30ce7f7110cb018e403614fe
Sagar Chandra <sagarchandra1824@gmail.com>
2026-08-05 14:27:18 +0530

    Time: 2 ms (56.04%) | Memory: 28 MB (21.38%) - LeetSync

---
+++ 143-reorder-list/reorder-list.py
@@ -0,0 +1,40 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        # Finding middle element
+        slow=fast=head
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+        curr=slow.next
+        slow.next=None
+        # reversing second half
+        prev=None
+        while curr:
+            nxxt=curr.next
+            curr.next=prev
+            prev=curr
+            curr=nxxt
+        
+        # putting both halfs alternatively
+        left=head
+        right=prev
+        while right:
+            l=left.next
+            r=right.next
+            left.next=right
+            right.next=l
+            left=l
+            right=r
+        return head
+        
+
+
+        

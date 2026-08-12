be4078c667177ce18a8c5befdf7c779fd90fd206
Sagar Chandra <sagarchandra1824@gmail.com>
2026-08-12 15:46:55 +0530

    Time: 5 ms (85.00%) | Memory: 23.9 MB (62.73%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -10,10 +10,10 @@ class Solution:
         def dfs(node):
             if not node:
                 return 0
-            left=max(0,dfs(node.left)) 
-            right=max(0,dfs(node.right))
-            self.ans=max(self.ans,node.val+left+right)
-            return node.val+max(left,right)
+            left=max(0,dfs(node.left))   # max path sum we can get from left subtree
+            right=max(0,dfs(node.right))  # max pathsum we can get from right subtree
+            self.ans=max(self.ans,node.val+left+right)  # doing left+node+right
+            return node.val+max(left,right)  # can only give either left or right from node if we move upwards towards its parents
         dfs(root)
         return self.ans
 

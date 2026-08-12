88087343d702fe4dc9599a599342f8d28fe06378
Sagar Chandra <sagarchandra1824@gmail.com>
2026-08-12 22:12:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (59.63%) - LeetSync

--- 476-number-complement/number-complement.py
+++ 476-number-complement/number-complement.py
@@ -1,12 +1,4 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-        res=bin(num)[2:]
-        com=""
-        for b in res:
-            if b=="1":
-                com+='0'
-            else:
-                com+='1'
-        return int(com,2)
-
-        
+        mask=(1<<num.bit_length())-1
+        return num^mask

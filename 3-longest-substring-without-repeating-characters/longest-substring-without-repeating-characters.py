9d9096a397fbeaf52c81611d35baa00d757a58f1
Sagar Chandra <sagarchandra1824@gmail.com>
2026-08-13 20:33:42 +0530

    Time: 319 ms (7.01%) | Memory: 19.8 MB (20.66%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -3,15 +3,15 @@ class Solution:
         seen={}
         left=0
         ans=0
-
         for right in range(len(s)):
-            seen[s[right]]=seen.get(s[right],0)+1
-            while seen[s[right]]>1:
+            ch=s[right]
+            seen[ch]=seen.get(ch,0)+1
+            while seen[ch]>1:
                 seen[s[left]]-=1
                 if seen[s[left]]==0:
                     del seen[s[left]]
                 left+=1
             ans=max(ans,right-left+1)
-           
-        return ans 
+        return ans
+
         

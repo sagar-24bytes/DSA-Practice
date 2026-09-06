f898e1613e4ed8031e8b1df9d430135638219d8d
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-06 19:06:00 +0530

    Time: 11 ms (82.70%) | Memory: 22.1 MB (64.64%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans=[]
         res={}
+
         for s in strs:
-            x="".join(sorted(s))
-            res.setdefault(x,[]).append(s)
-        for y in res.values():
-            ans.append(y)
-        return ans
+            st="".join(sorted(s))
+            res.setdefault(st,[]).append(s)
+        return list(res.values())
         

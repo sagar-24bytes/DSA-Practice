4bb97104b7e01f59f72e53fa45c4196a06ea623d
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-07 22:39:00 +0530

    Time: 3 ms (59.12%) | Memory: 21.1 MB (30.23%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,15 +1,17 @@
-from functools import lru_cache
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n=len(prices)
-        @lru_cache(None)
+        memo={}
         def profit(idx,buy):
             if idx<0 or idx>=n:
                 return 0
+            if (idx,buy) in memo:
+                return memo[(idx,buy)]
             if buy==1:
-                return max(-prices[idx]+profit(idx+1,0) , profit(idx+1,1))
+                memo[(idx,buy)]=max(-prices[idx]+profit(idx+1,0) , profit(idx+1,1))
             else:
-                return max(prices[idx]+profit(idx+2,1) , profit(idx+1,0))
+                memo[(idx,buy)]=max(prices[idx]+profit(idx+2,1) , profit(idx+1,0))
+            return memo[(idx,buy)]
             
         return profit(0,1)
 

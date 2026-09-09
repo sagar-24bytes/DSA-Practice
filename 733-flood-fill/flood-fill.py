5ee5cba7c1e6a6a541a7f161159ba2cfaed5796c
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-10 02:10:51 +0530

    Time: 0 ms (100.00%) | Memory: 20.2 MB (7.37%) - LeetSync

---
+++ 733-flood-fill/flood-fill.py
@@ -0,0 +1,23 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        rows=len(image)
+        cols=len(image[0])
+        original=image[sr][sc]
+        visited=[[False]*cols for i in range(rows)]
+        # if original==color:
+        #     return image
+        d=[(1,0),(-1,0),(0,1),(0,-1)]
+        def dfs(r,c):
+            image[r][c]=color
+            visited[r][c]=True
+            for x,y in d:
+                nr=r+x
+                nc=c+y
+                if 0<=nr<rows and 0<=nc<cols and image[nr][nc]==original and not visited[nr][nc]:
+                    dfs(nr,nc)
+                
+        dfs(sr,sc)
+        return image
+
+            
+        

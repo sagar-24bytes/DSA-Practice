e7328d1bf3d69801822f4ad60e9604fa14a6cc65
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-12 11:30:45 +0530

    Time: 4 ms (67.61%) | Memory: 22.1 MB (91.47%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -8,38 +8,26 @@ class Solution:
         rows=len(board)
         cols=len(board[0])
         visited=[[False]*cols for _ in range(rows)]
-
         for r in range(rows):
             for c in range(cols):
-                if board[r][c]=='O' and (r==0 or r==rows-1 or c==0 or c==cols-1):
-                    visited[r][c]=True
-                    q.append((r,c))
-        
-        d=[(-1,0),(1,0),(0,-1),(0,1)]
+                if r==0 or r==rows-1 or c==0 or c==cols-1:
+                    if board[r][c]=='O':
+                        q.append((r,c))
+                        visited[r][c]=True
+        d=[(-1,0),(1,0),(0,1),(0,-1)]
         while q:
             r,c=q.popleft()
             for x,y in d:
                 nr=r+x
                 nc=c+y
-                if 0<=nr<rows and 0<=nc<cols and board[nr][nc]=='O' and  not visited[nr][nc]:
+                if 0<=nr<rows and 0<=nc<cols and board[nr][nc]=='O' and not visited[nr][nc]:
                     visited[nr][nc]=True
                     q.append((nr,nc))
         for r in range(rows):
             for c in range(cols):
                 if board[r][c]=='O' and not visited[r][c]:
-                    
                     board[r][c]='X'
         
-
-
-
-
-
-       
         
-
-
-                    
-
 
         

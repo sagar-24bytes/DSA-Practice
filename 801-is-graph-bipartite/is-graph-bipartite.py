54d21297b08283f8bc6583737f32684c8bb3464e
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-13 14:06:33 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (30.10%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.py
+++ 801-is-graph-bipartite/is-graph-bipartite.py
@@ -2,23 +2,20 @@ class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         n=len(graph)
         color=[-1]*n
-        def dfs(node,curr_col):
-            color[node]=curr_col
+
+        def dfs(node,col):
+            color[node]=col
             for nei in graph[node]:
-                if color[nei]==-1:  # unvisited node
-                    if not dfs(nei,1-curr_col):
+                if color[nei]==-1:
+                    if not dfs(nei,1-col):
                         return False
-                else:   # already visited node
-                    if color[nei]==curr_col:
-                        return False
+                elif col==color[nei]:
+                    return False
             return True
-        
         for i in range(n):
-            if color[i]==-1:   # starting dfs only when node is not visited(coloured)
+            if color[i]==-1:
                 if not dfs(i,0):
                     return False
         return True
-
-
-
-        
+                    
+                    

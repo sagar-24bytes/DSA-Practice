50c729f2bac0dc03bf8d2ad53e5558bfc84ddef8
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-16 09:54:04 +0530

    Time: 98 ms (84.53%) | Memory: 22 MB (68.16%) - LeetSync

--- 542-01-matrix/01-matrix.py
+++ 542-01-matrix/01-matrix.py
@@ -5,23 +5,25 @@ class Solution:
         cols=len(mat[0])
         ans=[[-1]*cols for _ in range(rows)]
         q=deque()
-        d=[(-1,0),(1,0),(0,-1),(0,1)]
         for r in range(rows):
             for c in range(cols):
                 if mat[r][c]==0:
-                    q.append((r,c,0))
                     ans[r][c]=0
+                    q.append((r,c))
+        d=[(-1,0),(1,0),(0,-1),(0,1)]
         while q:
-            r,c,step=q.popleft()
+            r,c=q.popleft()
+
             for x,y in d:
                 nr=r+x
                 nc=c+y
                 if 0<=nr<rows and 0<=nc<cols:
                     if ans[nr][nc]==-1:
-                        ans[nr][nc]=step+1
-                        q.append((nr,nc,step+1))
+                        q.append((nr,nc))
+                    # if mat[nr][nc]!=0:
+                        ans[nr][nc]=ans[r][c]+1
+                        
         return ans 
-
-
+            
 
         

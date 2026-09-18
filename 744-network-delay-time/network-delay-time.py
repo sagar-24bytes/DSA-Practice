e7381f44616708a7477c6a2178e923d20c317be9
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-19 00:24:41 +0530

    Time: 27 ms (100.00%) | Memory: 21.8 MB (13.67%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -5,23 +5,22 @@ class Solution:
         for u,v,w in times:
             graph[u].append([v,w])
         dist=[float('inf')]*(n+1)
-        dist[0]=0
-        dist[k]=0
+        dist[0]=dist[k]=0
         heap=[]
         heapq.heappush(heap,(0,k))
-
         while heap:
             d,node=heapq.heappop(heap)
             for nei,w in graph[node]:
-                if d+w <dist[nei]:
+                if d+w<dist[nei]:
                     dist[nei]=d+w
-                    heapq.heappush(heap,(dist[nei], nei ))
-        for j in dist:
-            if j==float('inf'):
+                    heapq.heappush(heap,(dist[nei],nei))
+        for x in dist:
+            if x==float('inf'):
                 return -1
         return max(dist)
 
 
 
+        
 
         

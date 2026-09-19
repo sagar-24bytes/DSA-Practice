1571290978f839f2a735086c31c83bff529fb13f
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-19 17:42:03 +0530

    Time: 243 ms (57.93%) | Memory: 21.5 MB (92.32%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,26 +1,30 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         rows=len(grid)
         cols=len(grid[0])
         visited=[[False]*cols for _ in range(rows)]
-
-        def dfs(r,c):
-            if r<0 or r>=rows or c<0 or c>=cols:
-                return 
-            if grid[r][c]=='0' or visited[r][c]:
-                return 
+        d=[(-1,0),(1,0),(0,-1),(0,1)]
+        def bfs(r,c):
+            q=deque()
+            q.append((r,c))
             visited[r][c]=True
-            dfs(r-1,c)
-            dfs(r+1,c)
-            dfs(r,c-1)
-            dfs(r,c+1)
+            while q:
+                i,j=q.popleft()
+                for x,y in d:
+                    nr=i+x
+                    nc=j+y
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]=='1' and not visited[nr][nc]:
+                        visited[nr][nc]=True
+                        q.append((nr,nc))
+
         ans=0
         
         for  i in range(rows):
             for j in range(cols):
                 if grid[i][j]=='1' and not visited[i][j]:
                     ans+=1
-                    dfs(i,j)
+                    bfs(i,j)
         return ans
 
         

f0148ba188568908043b1dd515474b0723e9548b
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-22 17:54:44 +0530

    Time: 29 ms (19.59%) | Memory: 20.5 MB (29.39%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,9 +1,8 @@
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
+    def canJump(self, nums: list[int]) -> bool:
         reach=0
         for i in range(len(nums)):
             if i>reach:
                 return False
             reach=max(reach,i+nums[i])
         return True
-        

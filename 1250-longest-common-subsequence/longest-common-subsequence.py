93e781cf4913bb4c23d75bc91ab2589cd7c08396
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-22 17:02:21 +0530

    Time: 1275 ms (9.22%) | Memory: 316.4 MB (9.83%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -7,10 +7,10 @@ class Solution:
             if (i,j) in memo:
                 return memo[(i,j)]
             if text1[i]==text2[j]:
-                memo[(i,j)]=1+func(i-1,j-1)
+                memo[(i,j)]=1 + func(i-1,j-1)
             else:
-                memo[(i,j)]=max(func(i,j-1),func(i-1,j))
+                memo[(i,j)]=max(func(i-1,j) , func(i,j-1))
             return memo[(i,j)]
-        return func(len(text1)-1,len(text2)-1)
+        return func(len(text1)-1 , len(text2)-1)
 
         

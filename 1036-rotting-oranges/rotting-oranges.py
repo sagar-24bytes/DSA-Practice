b4e251510d8a32af58653d64bb5123fba0034c06
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-24 14:43:52 +0530

    Time: 2 ms (80.95%) | Memory: 19.4 MB (43.48%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,35 +1,34 @@
 from collections import deque
 class Solution:
-    def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh=0
-        ans=0
+    def orangesRotting(self, grid: list[list[int]]) -> int:
+        q=deque()
         rows=len(grid)
         cols=len(grid[0])
-        q=deque()
-        visited=[[False]*cols for _ in range(rows)]
+        fresh=0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c]==2:
                     q.append((r,c))
                 elif grid[r][c]==1:
                     fresh+=1
-        if fresh==0:
-            return 0
-        d=[(1,0),(-1,0),(0,1),(0,-1)]
+        ans=0
+        d=[(-1,0),(1,0),(0,-1),(0,1)]
         while q and fresh>0:
-            for i in range(len(q)):
+            n=len(q)
+            for i in range(n):
                 r,c=q.popleft()
                 for x,y in d:
                     nr=r+x
                     nc=c+y
-                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==1:
+                    if nr<0 or nr>=rows or 0>nc or nc>=cols:
+                        continue
+                    if grid[nr][nc]==0 or grid[nr][nc]==2:
+                        continue
+                    if grid[nr][nc]==1:
                         fresh-=1
                         grid[nr][nc]=2
                         q.append((nr,nc))
             ans+=1
         return ans if fresh==0 else -1
-            
-            
-            
 
-        
+                

2d68e4c2092b0f9aee9b62c693c0ff6d636f657c
Sagar Chandra <sagarchandra1824@gmail.com>
2026-09-24 14:58:17 +0530

    Time: 3 ms (85.80%) | Memory: 20.5 MB (63.05%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,28 +1,29 @@
+from collections import deque
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
         graph=[[] for _ in range(numCourses)]
-        for a,b in prerequisites:
-            graph[b].append(a)
-        visited=[False]*numCourses
-        path=[False]*numCourses
-
-        def dfs(node):   # if cycle exists in directed graph -> ans=False , warna ans= True here
-            visited[node]=True
-            path[node]=True
-            for nei in graph[node]:
-                if path[nei]:
-                    return False
-                elif not visited[nei]:
-                    if not dfs(nei):
-                        return False
-            path[node]=False
-            return True
+        indegree=[0]*(numCourses)
+        for u,v in prerequisites:
+            graph[v].append(u)
+            indegree[u]+=1
+        q=deque()
         for i in range(numCourses):
-            if not visited[i]:
-                if not dfs(i):
-                    return False
-        return True
+            if indegree[i]==0:
+                q.append(i)
+        res=[]
+        # print(indegree)
+        while q:
+            node=q.popleft()
+            res.append(node)
+            for nei in graph[node]:
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    q.append(nei)
+        # print(res)
+        return len(res)==numCourses
+                
 
         
         
+
         
